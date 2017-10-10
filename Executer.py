088cc1fcf0744dc28c3e7ca29dcc191c2640f768
Vinícius <vfulber@inf.ufsm.br>
2017-10-10 16:03:51 -0300

    Executer functions

--- Executer.py
+++ Executer.py
@@ -1,8 +1,10 @@
 from Parser import PlatformParser
+from mininet.net import Mininet
+from mininet.cli import CLI
 
 class Executer:
     CONFIGURATION = None
-    MININETCOMPONENTS = []
+    NET = None
 
     def __init__(self, CONFIGURATION):
 
@@ -10,11 +12,42 @@ class Executer:
 
 #------------------------------------------------------------------
 
-    def VNFSStart(self):
+    def VNFSPrepare(self):
 
         for VNFINSTANCE in self.CONFIGURATION.VNFS:
             VNFINSTANCE.createVNF()
+
+#------------------------------------------------------------------
+
+    def mininetPrepare(self):
+
+        self.NET = Mininet()
+
+        for HOST in self.CONFIGURATION.MNHOSTS:
+            HOST.ELEM = self.NET.addHost(HOST.ID)
+            HOST.ELEM.setIP(HOST.IP)
+
+        for SWITCH in self.CONFIGURATION.MNSWITCHES:
+            SWITCH.ELEM = self.NET.addSwitch(SWITCH.ID)
+
+        for CONTROLLER in self.CONFIGURATION.MNCONTROLLERS:
+            CONTROLLER.ELEM = self.NET.addController(CONTROLLER.ID)
+
+        for OVS in self.MNOVSES:
+            OVS.ELEM = self.NET.addSwitch(OVS.ID)
+
+#------------------------------------------------------------------
+
+    def connectionsPrepare(self):
+        print ("NOT IMPLEMENTED YET")
+
+#------------------------------------------------------------------
+
+    def topologyStart(self):
+
+        for VNFINSTANCE in self.CONFIGURATION.VNFS:
             VNFINSTANCE.upVNF()
+        self.NET.start()
 
 #------------------------------------------------------------------
 
